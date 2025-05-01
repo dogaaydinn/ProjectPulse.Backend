c2b4c6a101543d5b963e36f45506a0de2fda4d7d
doga <dogaa882@gmail.com>
2025-05-01 16:32:57 +0300

    fix(shared): align IEntity interface and BaseEntity Id access modifiers

--- src/Shared/Base/BaseEntity.cs
+++ src/Shared/Base/BaseEntity.cs
@@ -1,6 +1,18 @@
+using Domain.Common;
+
 namespace Shared.Base;
 
 public abstract class BaseEntity : IEntity
 {
     public Guid Id { get; protected set; } = Guid.NewGuid();
+
+    private readonly List<IDomainEvent> _domainEvents = new();
+    public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    protected void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public void ClearDomainEvents() => _domainEvents.Clear();
 }
