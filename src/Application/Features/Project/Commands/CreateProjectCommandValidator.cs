a1c2c8e6307ba98fe3921250b4f42195eedaf57d
doga <dogaa882@gmail.com>
2025-05-01 18:34:25 +0300

    feat(application): update CreateProjectCommandValidator to use shared validation messages for improved consistency

--- src/Application/Features/Project/Commands/CreateProjectCommandValidator.cs
+++ src/Application/Features/Project/Commands/CreateProjectCommandValidator.cs
@@ -1,3 +1,5 @@
+using Shared.Constants;
+
 namespace Application.Features.Project.Commands;
 
 public class CreateProjectCommandValidator : AbstractValidator<CreateProjectCommand>
@@ -5,14 +7,8 @@ public class CreateProjectCommandValidator : AbstractValidator<CreateProjectComm
     public CreateProjectCommandValidator()
     {
         RuleFor(x => x.Name)
-            .NotEmpty().WithMessage("Project name is required.")
-            .MaximumLength(100);
-
-        RuleFor(x => x.ManagerId)
-            .NotEmpty().WithMessage("Manager ID is required.");
-
-        RuleFor(x => x.StartDate)
-            .LessThanOrEqualTo(DateTime.UtcNow.AddYears(1))
-            .WithMessage("Start date cannot be more than 1 year in the future.");
+            .NotEmpty()
+            .WithMessage(ValidationMessages.ProjectNameRequired);
+        
     }
 }
