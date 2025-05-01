02ee80478828df662f161cfaa5ffd0f998e23bd6
doga <dogaa882@gmail.com>
2025-05-01 16:11:11 +0300

    refactor(shared): switch audit fields to Guid for consistency with user ID strategy

--- src/Shared/Base/IAuditableEntity.cs
+++ src/Shared/Base/IAuditableEntity.cs
@@ -2,5 +2,8 @@ namespace Shared.Base;
 
 public interface IAuditableEntity
 {
-    
+    DateTime? CreatedDate { get; set; }
+    Guid? CreatedBy { get; set; }
+    DateTime? UpdatedDate { get; set; }
+    Guid? UpdatedBy { get; set; }
 }
