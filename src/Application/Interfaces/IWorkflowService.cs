82e6bd5140ff610015bd1b1e0362c47fb9833561
doga <dogaa882@gmail.com>
2025-05-03 20:14:53 +0300

    feat(workflow): add CreateWorkflowRequest DTO and IWorkflowService interface with workflow management methods

---
+++ src/Application/Interfaces/IWorkflowService.cs
@@ -0,0 +1,12 @@
+using Application.DTOs;
+using Shared.Results;
+
+namespace Application.Interfaces;
+
+public interface IWorkflowService
+{
+    Task<Result<Guid>> CreateWorkflowAsync(CreateWorkflowRequest request);
+    Task<Result<WorkflowDto>> GetWorkflowByIdAsync(Guid workflowId);
+    Task<Result<List<WorkflowDto>>> GetWorkflowsByProjectIdAsync(Guid projectId);
+    Task<Result> DeleteWorkflowAsync(Guid workflowId);
+}
